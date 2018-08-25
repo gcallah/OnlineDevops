a0b4d0bfa77954159fbbe60e636581d5c43c13d4
Gene Callahan <gcallah@mac.com>
2018-08-25 21:39:16 +0000

    Got static files working on PA.

---
+++ devops/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'devops'  # type: str
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/*$', views.about, name='about'),
+]
