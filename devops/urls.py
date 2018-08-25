f9f14b678a160962b86fac88bae8ab359b2a33e3
gcallah <gcallah@mac.com>
2018-08-25 18:09:40 -0400

    Fixed error with about.html.

--- devops/urls.py
+++ devops/urls.py
@@ -7,5 +7,5 @@ app_name = 'devops'  # type: str
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/*$', views.about, name='about'),
+    url(r'^devops/about/*$', views.about, name='about'),
 ]
