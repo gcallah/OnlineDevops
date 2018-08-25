80076918c564b1cc71ce454efc80fdd7eaebb38d
gcallah <gcallah@mac.com>
2018-08-25 17:56:30 -0400

    Got static working locally.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "mysite", "static", ),
+    )
 STATIC_ROOT = "/home/devopscourse/OnlineDevops/mysite/static"
