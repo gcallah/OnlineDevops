1e89b1149ddfe2599f91d72c697767a7d97fa014
gcallah <gcallah@mac.com>
2018-08-25 21:50:28 -0400

    Added rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# for the dev server: fetches new code and restarts the server.
+
+git pull origin master
+echo "going to reboot the webserver"
+touch /var/www/www_thedevopscourse_com_wsgi.py
