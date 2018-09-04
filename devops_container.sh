6a2ef8246b9bf157acf3ac5549cf1134e7a43f43
Akshay Tambe <apt321@nyu.edu>
2018-09-04 14:47:34 -0400

    Changed docker script to use locally built image.

--- devops_container.sh
+++ devops_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker rm devops || true
-docker run -it -p 8000:8000 -v $PWD:/home/DevOps --name devops akshaytambe/devops:v1 bash
+docker run -it -p 8000:8000 -v $PWD:/home/DevOps --name devops bash
