7faedc82ca0e8782a73f0f83b392d1e09bb3cced
Akshay Tambe <apt321@nyu.edu>
2018-09-04 15:00:05 -0400

    Powershell Script Modification: Removed Name Flag

--- devops_container.ps1
+++ devops_container.ps1
@@ -1,3 +1,3 @@
 # Windows Powershell Script for DevOps Docker Container
 docker rm devops | true
-docker run -it -p 8000:8000 -v ${PWD}:/home/DevOps --name devops akshaytambe/devops:v1 bash
+docker run -it -p 8000:8000 -v ${PWD}:/home/DevOps devops bash
