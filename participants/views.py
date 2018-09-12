6de17f17617fbe9776a82cfab76d64ed10ea220c
Denis Petelin <d.petelin@icloud.com>
2018-09-12 15:55:38 -0400

    For authenticated users - login is displayed in top right corner, for non-authenticated - logout.

--- participants/views.py
+++ participants/views.py
@@ -5,6 +5,11 @@ from django.http import HttpResponse
 
 @login_required
 def home(request):
+    """
+    This view demonstrates how access can be restricted when we need user name to collect
+    :param request: browser request as HttpRequest()
+    :returns: welcome message with username in it as HttpResponse()
+    """
     logged_in_user = request.user.__str__()
     message = "Welcome, " + logged_in_user + "! You now seeing restricted content..."
     return HttpResponse(message)
