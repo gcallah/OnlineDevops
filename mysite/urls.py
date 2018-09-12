cbfc528a1a1ea6ac0304b162e5c97d3be811109d
Denis Petelin <d.petelin@icloud.com>
2018-09-12 12:14:42 -0400

    Participants app is registered within global site namespace, default Participant Home added (now empty).

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('devops.urls')),
+    url('participants/', include('participants.urls'))
 ]
--- participants/views.py
+++ participants/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello World!")
