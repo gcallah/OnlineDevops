5991adea82039b524b79ee293fe2385066c1f113
gcallah <gcallah@mac.com>
2018-09-12 17:09:26 -0400

    Almost done.

--- qextract.py
+++ qextract.py
@@ -11,7 +11,6 @@ Functions:
 
 import os
 import sys
-import csv
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
@@ -26,12 +25,9 @@ def read_records(mod_nm):
         recs = Question.objects.values()
     else:
         recs = Question.objects.filter(module=mod_nm).values()
-    # next two lines are just for debugging:
-    for rec in recs:
-            print(rec)
     return recs
 
-def write_records(filenm, recs):
+def write_records(recs):
     """
         Args:
             filenm: where to output the CSV
@@ -39,30 +35,25 @@ def write_records(filenm, recs):
         Returns:
             None (for now: we probably want success or error codes)
     """
-    with open(filenm, "w") as f_out:
-        fwriter = csv.writer(f_out)
-        for record in recs:
-            output = []
-            for (key, val) in record.items():
-                # next line just for debugging:
-                print("Key = " + str(key) + "; Val = " + str(val))
-                if key in desired_flds:
-                    output.append(val)
-            fwriter.writerow(output)
+    i = 1
+    for question in recs:
+        print(str(i) + ". (1 point)")
+        print(question["text"])
+        print()
+        # the answers get printed here: * the correct one
+        print("a. " + question["answerA"])
+        print("b. " + question["answerB"])
+        # remember to make sure the rest aren't blank!
+        i += 1
+        print()
 
 def main():
     mod_nm = None
-    outf = None
-    if len(sys.argv) < 2:
-        print("You must supply an output file name.")
-        exit(1)
-    else:
-        outf = sys.argv[1]
-    if len(sys.argv) > 2:
-        mod_nm = sys.argv[2]
+    if len(sys.argv) > 1:
+        mod_nm = sys.argv[1]
 
     recs = read_records(mod_nm)
-    write_records(outf, recs)
+    write_records(recs)
 
 if __name__ == '__main__':
     main()
