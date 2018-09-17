00a42996b4f8d916d42e63f0bf69064109ae81b8
Denis Petelin <d.petelin@icloud.com>
2018-09-17 14:01:51 -0400

    Grades are now saved for those who are logged in when taking a Quiz, along with the Quiz/Module name, and date when Grade was recorded.

--- devops/views.py
+++ devops/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import request, HttpRequest, Http404, HttpResponseServerError, HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from decimal import Decimal
 
-from .models import Question
+from .models import Question, Grade, Quiz
 
 site_hdr = "The DevOps Course"
 
@@ -102,73 +103,77 @@ def grade_quiz(request: HttpRequest()) -> list:
                     proper_id = str(key).strip('_')
                     user_answers.append({proper_id: value})
 
-                # forces user to answer all quiz questions, redirects to module page if not completed
-                # TODO: keep previously selected radio buttons checked instead of clearing form
-                mod_nm = form_data['submit']
-                num_ques = Question.objects.filter(module=mod_nm).count()
-                if len(user_answers) != num_ques:
-                    messages.warning(request, 'Please complete all questions before submitting')
-                    return redirect('/devops/' + mod_nm)
+            # forces user to answer all quiz questions, redirects to module page if not completed
+            # TODO: keep previously selected radio buttons checked instead of clearing form
+            mod_nm = form_data['submit']
+            num_ques = Question.objects.filter(module=mod_nm).count()
+            if len(user_answers) != num_ques:
+                messages.warning(request, 'Please complete all questions before submitting')
+                return redirect('/devops/' + mod_nm)
 
-                # now get those answers from database & check if answer is right...
-                for answered_question in user_answers:
-                    processed_answer = {}
-                    id_to_retrieve = next(iter(answered_question))
-                    original_question = get_object_or_404(Question, pk=id_to_retrieve)
+            # now get those answers from database & check if answer is right...
+            for answered_question in user_answers:
+                processed_answer = {}
+                id_to_retrieve = next(iter(answered_question))
+                original_question = get_object_or_404(Question, pk=id_to_retrieve)
 
-                    # Lets start building a dict with the status for this particular question...
-                    # Following the DRY principle - here comes shared part for both cases...
-                    processed_answer['question'] = original_question.text
-                    processed_answer['correctAnswer'] = original_question.correct
-                    processed_answer['yourAnswer'] = answered_question[id_to_retrieve]
+                # Lets start building a dict with the status for this particular question...
+                # Following the DRY principle - here comes shared part for both cases...
+                processed_answer['question'] = original_question.text
+                processed_answer['correctAnswer'] = original_question.correct
+                processed_answer['yourAnswer'] = answered_question[id_to_retrieve]
 
-                    correctanskey = "answer{}".format(processed_answer['correctAnswer'].upper())
-                    youranskey = "answer{}".format(processed_answer['yourAnswer'].upper())
+                correctanskey = "answer{}".format(processed_answer['correctAnswer'].upper())
+                youranskey = "answer{}".format(processed_answer['yourAnswer'].upper())
 
-                    processed_answer['correctAnswerText'] = getattr(original_question, correctanskey)
-                    processed_answer['yourAnswerText'] = getattr(original_question, youranskey)
+                processed_answer['correctAnswerText'] = getattr(original_question, correctanskey)
+                processed_answer['yourAnswerText'] = getattr(original_question, youranskey)
 
-                    # and now we are evaluating either as right or wrong...
-                    if answered_question[id_to_retrieve] == original_question.correct:
-                        processed_answer['message'] = "Congrats, thats correct!"
-                        processed_answer['status'] = "right"
-                        num_ques_correct += 1
-                    else:
-                        processed_answer['message'] = "Sorry, that's incorrect!"
-                        processed_answer['status'] = "wrong"
-
+                # and now we are evaluating either as right or wrong...
+                if answered_question[id_to_retrieve] == original_question.correct:
+                    processed_answer['message'] = "Congrats, thats correct!"
+                    processed_answer['status'] = "right"
+                    num_ques_correct += 1
+                else:
+                    processed_answer['message'] = "Sorry, that's incorrect!"
+                    processed_answer['status'] = "wrong"
                     # and store to ship to the Template.
-                    graded_answers.append(processed_answer)
+                graded_answers.append(processed_answer)
 
-                # Calculating quiz score
-                num_ques_correct_percentage = int((num_ques_correct / num_ques) * 100)
+            # Calculating quiz score
+            num_ques_correct_percentage = Decimal((num_ques_correct / num_ques) * 100)
 
-                # Pass a quiz name to view & display at Here are your quiz results
-                quiz_name = {
-                    'work': 'MOD1: The DevOps Way of Work',
-                    'comm': 'MOD2: Cooperation and Communication (Tool: Slack)',
-                    'incr': 'MOD3: Incremental Development (Tool: git)',
-                    'build': 'MOD4: Automating Builds (Tool: make)',
-                    'flow': 'MOD5: Workflow (Tool: kanban boards)',
-                    'test': 'MOD6: Automating Testing (Tool: Jenkins)',
-                    'infra': 'MOD7: Software as Infrastructure (Tool: Docker)',
-                    'cloud': 'MOD8: Cloud Deployment (Tool: Kubernetes)',
-                    'micro': 'MOD9: Microservices and Serverless Computing',
-                    'monit': 'MOD10: Monitoring (Tool: StatusCake)',
-                    'secur': 'MOD11: Security',
-                    'sum': 'MOD12: Summing Up',
-                }
+            # Pass a quiz name to view & display at Here are your quiz results
+            quiz_name = {
+                'work': 'MOD1: The DevOps Way of Work',
+                'comm': 'MOD2: Cooperation and Communication (Tool: Slack)',
+                'incr': 'MOD3: Incremental Development (Tool: git)',
+                'build': 'MOD4: Automating Builds (Tool: make)',
+                'flow': 'MOD5: Workflow (Tool: kanban boards)',
+                'test': 'MOD6: Automating Testing (Tool: Jenkins)',
+                'infra': 'MOD7: Software as Infrastructure (Tool: Docker)',
+                'cloud': 'MOD8: Cloud Deployment (Tool: Kubernetes)',
+                'micro': 'MOD9: Microservices and Serverless Computing',
+                'monit': 'MOD10: Monitoring (Tool: StatusCake)',
+                'secur': 'MOD11: Security',
+                'sum': 'MOD12: Summing Up',
+            }
 
-                # now we are ready to record quiz results...
+            # now we are ready to record quiz results...
+            if request.user:
+                action_status = Grade.objects.create(participant=request.user,
+                                                     score=num_ques_correct_percentage.real,
+                                                     quiz=Quiz.objects.get(module=mod_nm),
+                                                     quiz_name=mod_nm)
 
+            # ok, all questions processed, lets render results...
+            return render(request, 'graded_quiz.html', dict(graded_answers=graded_answers,
+                                                            num_ques=num_ques,
+                                                            num_ques_correct=num_ques_correct,
+                                                            num_ques_correct_percentage=int(num_ques_correct_percentage),
+                                                            quiz_name=quiz_name.get(mod_nm),
+                                                            header=site_hdr))
 
-                # ok, all questions processed, lets render results...
-                return render(request, 'graded_quiz.html', dict(graded_answers=graded_answers,
-                                                                num_ques=num_ques,
-                                                                num_ques_correct=num_ques_correct,
-                                                                num_ques_correct_percentage=num_ques_correct_percentage,
-                                                                quiz_name=quiz_name.get(mod_nm),
-                                                                header=site_hdr))
 
         # If it is PUT, DELETE etc. we say we dont do that...
         else:
@@ -179,5 +184,3 @@ def grade_quiz(request: HttpRequest()) -> list:
         return HttpResponseServerError(e.__cause__,
                                        e.__context__,
                                        e.__traceback__)
-
-
