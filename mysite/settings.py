db348accfb1ef0686cdab206df6d7082ca619568
Denis Petelin <d.petelin@icloud.com>
2018-09-21 19:10:16 -0400

    Ready for staging - all environment vars in place.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '@+%x+r-cfv@*2+qi)&9w^6jvp-h=bypm+7%=qvcjw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
