7d79b7d33b0d6b8b02b83c1a0d9841205ec8d01a
“Denis <denis@nyu.edu>
2018-09-24 13:30:59 -0400

    Test suite for the DC-3 User Auth. This gives us coverage of 80%, giving a solid probability of knowing if main success scenario is broken.
    Name                     Stmts   Miss Branch BrPart  Cover
    devops/models.py            32      2      0      0    94%
    devops/views.py             86     23     14      2    75%
    participants/admin.py        0      0      0      0   100%
    participants/models.py       0      0      0      0   100%
    participants/views.py        6      0      0      0   100%
    ----------------------------------------------------------
    TOTAL                      124     25     14      2    80%

--- devops/tests/test_integration.py
+++ devops/tests/test_integration.py
@@ -224,3 +224,31 @@ class UserAuthenticationTestCase(TestCase):
     """
     This testcase contains set of integration tests for DC-3 User Auth
     """
+    def setUp(self):
+        self.username = 'test_runner'
+        self.password = 'dumb_runner'
+        self.client = Client()
+
+        User.objects.create_user(username=self.username, email='nobody@nowhere.com', password=self.password)
+
+    def test_restricted_content_redirects_user_to_login(self):
+        response = self.client.get(reverse('participant_home'))
+        self.assertRedirects(response, reverse('participant_login')+'?next='+reverse('participant_home'))
+
+    def test_authenticated_user_redirects_to_index_logout_available(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse('devops:index'))
+        expected_html = str('<a href="' + reverse('participant_logout') + '">Log out</a>')
+        self.assertInHTML(expected_html, str(response.content) )
+
+    def test_authenticed_user_can_view_restricted_content(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse('participant_home'))
+        expected_answer = "<p>Welcome, " + self.username + "! You now seeing restricted content...</p>"
+        self.assertInHTML(expected_answer, str(response.content))
+
+    def test_unregistered_user_cannot_access_restricted_content(self):
+        self.client.login(username='nonexistent', password='invalid')
+        response = self.client.get(reverse('participant_home'))
+        self.assertRedirects(response, reverse('participant_login') + '?next=' + reverse('participant_home'))
+
