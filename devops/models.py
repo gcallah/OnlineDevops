85170d087ee966e840d54f79bd78c1743556ea40
Mandy Kong <mk5506@nyu.edu>
2018-10-05 00:05:25 -0400

    created module entity

--- devops/models.py
+++ devops/models.py
@@ -13,6 +13,11 @@ QTYPES = (
     ('ESSAY', 'Essay'),
 )
 
+class Module(models.Model):
+    module = models.CharField(max_length=MODNAM_LEN)
+    title = models.TextField()
+    next_module = models.CharField(max_length=MODNAM_LEN)
+
 class Quiz(models.Model):
     module = models.CharField(max_length=MODNM_LEN)
     minpass = models.FloatField(default=DEF_PASS)
