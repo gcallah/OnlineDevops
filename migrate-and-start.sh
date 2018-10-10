42779878b2a4bfd0c548fb5332c5c2eb72c5d4cf
shawn <shawn.widjaja@gmail.com>
2018-10-09 23:14:06 -0400

    mysql and django container creation/migration is fully automated now. user just needs to run docker compose up. will be improved to migrate data and start server as soon as mysql is ready instead of waiting an arbitrary 90 seconds as it does now

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -2,12 +2,15 @@
 
 # migration
 
-echo "Apply database migrations"
+echo "Waiting for MySQL to come up"
 
+sleep 90
+
+echo "Apply database migrations"
 # python manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 4 > datadump.json
-# exec python /home/DevOps/manage.py makemigrations
-# exec python /home/DevOps/manage.py migrate
-# exec python /home/DevOps/manage.py loaddata /home/DevOps/datadump.json
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata /home/DevOps/datadump.json
 
 echo "Starting server"
-exec python /home/DevOps/manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
--- startserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it devopsapp bash -c 'python manage.py makemigrations && python manage.py migrate && python manage.py loaddata datadump.json && python manage.py runserver 0.0.0.0:8000'
