5a9fda8cdc31f113fe626246041180cc696c87d0
Adam Sanghera <adamsanghera@gmail.com>
2018-11-06 01:56:15 -0500

    fixing misspellings, adding spellchecker to test_html.sh, with a check for interactive vs non-interactive to tolerate jenkins builds

--- test_html.sh
+++ test_html.sh
@@ -8,8 +8,14 @@ export TEST_DIR="devops/templates"
 export CODE_DIR="utils"
 export DATA_DIR="$CODE_DIR/data"
 
-for test_file in $TEST_DIR/*.html; 
+for test_file in $TEST_DIR/*.html;
 do
-    echo 'Checking file:' $test_file 
-    python3 $CODE_DIR/html_checker.py "$test_file" 
+    echo 'Checking file:' $test_file
+    python3 $CODE_DIR/html_checker.py "$test_file"
+    if [ -z $PS1 ]
+    then
+        python3 $CODE_DIR/html_spell.py -i $test_file $DATA_DIR/main-dict.txt $DATA_DIR/custom-dict.txt
+    else
+        python3 $CODE_DIR/html_spell.py $test_file $DATA_DIR/main-dict.txt $DATA_DIR/custom-dict.txt
+    fi
 done
