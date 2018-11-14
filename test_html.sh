6525d893c63e454e32efb41c5b03b766576f9164
gcallah <gcallah@mac.com>
2018-11-14 15:21:25 -0500

    Improved messages for test_html.sh

--- test_html.sh
+++ test_html.sh
@@ -10,8 +10,9 @@ export DATA_DIR="$CODE_DIR/data"
 
 for test_file in $TEST_DIR/*.html;
 do
-    echo 'Checking file:' $test_file
+    echo 'Html-checking file:' $test_file
     python3 $CODE_DIR/html_checker.py "$test_file"
+    echo 'Spell-checking file:' $test_file
     if [ -z $PS1 ]
     then
         python3 $CODE_DIR/html_spell.py -i $test_file $DATA_DIR/main-dict.txt $DATA_DIR/custom-dict.txt
