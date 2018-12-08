1f62576cdc10840442e34c721703d1d80796e67a
xiangcui <xc1008@nyu.edu>
2018-12-08 00:22:39 -0500

    refractor all js in html to seperate js file

--- mysite/static/devops/js/valid_search_submission.js
+++ mysite/static/devops/js/valid_search_submission.js
@@ -1,7 +1,7 @@
 var form = document.getElementById('formID'); // form has to have ID: <form id="formID">
 form.noValidate = true;
 form.addEventListener('submit', function(event) { // listen for form submitting
-    if (!event.target.checkValidity()) {
+    if (!event.target.checkValidity()){
         event.preventDefault();
         alert('Please answer all questions.'); // error message
     }
