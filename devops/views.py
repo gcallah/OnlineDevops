7981e849ea53ba67282c5270c6be24e6736403c1
pulkitaneja95 <pulkitaneja95@gmail.com>
2019-04-08 04:19:02 +0000

    adding dynamic page to load chapters from db

--- devops/views.py
+++ devops/views.py
@@ -67,7 +67,6 @@ def get_pg_w_quiz(request, mod_nm):
         rand_qs = []
         questions = Question.objects.filter(module=mod_nm)
         num_questions = questions.count()
-
         num_qs_to_randomize = DEF_NUM_RAND_QS
 
         if num_questions > 0:
@@ -100,12 +99,49 @@ def get_pg_w_quiz(request, mod_nm):
                        'mod_nm': mod_nm})
 
 
+
 def index(request: request) -> object:
     return render(request, 'index.html', {'header': site_hdr})
 
 
 def about(request: request) -> object:
     return render(request, 'about.html', {'header': site_hdr})
+
+
+def lesson(request, lesson='work'):
+    try:
+        contents = CourseModule.objects.get(module=lesson)
+        rand_qs = []
+        questions = Question.objects.filter(module=lesson)
+        num_questions = questions.count()
+        num_qs_to_randomize = DEF_NUM_RAND_QS
+
+        if num_questions > 0:
+            # we have to fetch numq from here:
+            quizzes = Quiz.objects.filter(module=lesson)
+            # we should log if we get count > 1 here!
+            for quiz in quizzes:
+                # we should have only 1 if any!
+                num_qs_to_randomize = quiz.numq
+                break
+            if num_questions >= num_qs_to_randomize:
+                rand_qs = random.sample(list(questions), num_qs_to_randomize)
+            else:
+                rand_qs = random.sample(list(questions), num_questions)
+
+        return render(request, 'dynamicpage.html', {
+            'header': site_hdr,
+            'questions': rand_qs,
+            'content': contents.content,
+            'mod_nm': lesson
+
+        })
+    except Exception:
+        return render(request, 'dynamicpage.html', {
+            'header': site_hdr,
+            'content': "Error! Please try again"})
+
+
 
 
 def gloss(request: request) -> object:
