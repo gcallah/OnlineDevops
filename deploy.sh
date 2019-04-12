2ed4c0a5f8f2683efc253e9257e90ce4b13f9edc
gcallah <gcallah@mac.com>
2019-04-11 23:26:49 -0400

    Changed key location for spleoy.sh.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 # This shell file deploys a new version to our server.
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 id_rsa # Allow read access to the private key
+ssh-add id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
 ssh devopscourse@ssh.pythonanywhere.com << EOF
