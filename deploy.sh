c12d91f39ef4f67a3d479131b58645e4714814e5
gcallah <gcallah@mac.com>
2019-04-12 00:17:19 -0400

    Trying to add known hosts through git.

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
-echo "Host *\n   StrictHostKeyChecking no" >> ~/.ssh/config
-
 
 echo "SSHing to PythonAnywhere."
 ssh devopscourse@ssh.pythonanywhere.com << EOF
