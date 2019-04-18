8f2877a9d90f8b16c1a3fc25699dd5aec81dbac6
gcallah <gcallah@mac.com>
2019-04-18 13:02:20 -0400

    Restoring rebuild script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# for the dev server: fetches new code and restarts the server.
+# for the prod server: fetches new code and restarts the server.
 
 git pull origin master
 echo "going to reboot the webserver"
 touch /var/www/www_thedevopscourse_com_wsgi.py
-python manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 4 > datadump_mysql1.json
