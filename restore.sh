ef87e5f0fb40badc30f3c6581f87649db33a1e0f
Faiyaz Sundrani <faiyaz.sundrani@gmail.com>
2019-04-24 11:33:29 -0400

    Added comments about the file
    Comments on how to execute the script

--- restore.sh
+++ restore.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #This file is to restore the database
 #It takes the name of the file to restore as command line argument
-#The Dataed backups of data is in OnlineDevops/backups folder
+#The Dated backups of data is in OnlineDevops/backups folder
+#To run this script ./restore.sh backups/filename.json
 
 #export USE_MYSQL=""
 restore_file="$1"
