c1e7d3ee01c08ed55f0bec948247d6eab67dd25e
Faiyaz Sundrani <faiyaz.sundrani@gmail.com>
2019-04-30 08:55:39 -0400

    Command to print the files to be deleted

--- delete_oldFiles.sh
+++ delete_oldFiles.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Command to delete files older than 5 days
 
+#Command to print the files that will be deleted
+find /home/devopscourse/OnlineDevops/backups/ -mindepth 1 -mtime +5 -depth -print
+
+#Command to delete files
 find /home/devopscourse/OnlineDevops/backups/ -mindepth 1 -mtime +5 -delete
