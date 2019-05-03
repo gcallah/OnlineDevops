a57bf322cad27558201f8071e7f9021458c71b7f
Dhaval Popat <dhavalpopat10@gmail.com>
2019-05-03 00:34:42 -0400

    Added URLs back in suite and updated html tests

--- tests/html_tests.sh
+++ tests/html_tests.sh
@@ -14,11 +14,8 @@ do
     then
         echo "Skipping URL check for local tests."
     else
-        if [ $test_file != "$TEST_DIR/suite.html" ]
-        then
-            echo 'URL-checking file:' $test_file
-            python3 $CODE_DIR/url_checker.py "$test_file" "http://www.thedevopscourse.com"
-        fi
+        echo 'URL-checking file:' $test_file
+        python3 $CODE_DIR/url_checker.py "$test_file" "http://www.thedevopscourse.com"
     fi
     echo 'Html-checking file:' $test_file
     python3 $CODE_DIR/html_checker.py "$test_file"
