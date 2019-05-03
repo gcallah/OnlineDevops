d1862fc3ba5c52369b466d175892f7756f7e1ead
gcallah <gcallah@mac.com>
2019-05-03 13:43:19 -0400

    Added comment in html_tests.sh.

--- tests/html_tests.sh
+++ tests/html_tests.sh
@@ -21,6 +21,7 @@ do
     python3 $CODE_DIR/html_checker.py "$test_file"
     echo 'Spell-checking file:' $test_file
     echo ''
+# Why is spell checker not being run?
 #    if [ -z $PS1 ]
 #    then
 #        python3 $CODE_DIR/html_spell.py -i $test_file $DATA_DIR/main-dict.txt $DATA_DIR/custom-dict.txt
