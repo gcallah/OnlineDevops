8e47b8e5f1c72808379811d032d00e1c84532002
gcallah <gcallah@mac.com>
2022-10-31 18:39:42 -0400

    Working on deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # This shell file deploys a new version to our server.
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
-ssh-add ~/.ssh/id_rsa # Add the private key to SSH
+if [ -z "$PA_PWD" ]
+then
+    echo "The PythonAnywhere password var (PA_PWD) must be set in the env."
+    exit 1
+fi
 
 echo "SSHing to PythonAnywhere."
-ssh devopscourse@ssh.pythonanywhere.com << EOF
-    cd /home/devopscourse/OnlineDevops
-    /home/devopscourse/OnlineDevops/rebuild.sh
+sshpass -p $PA_PWD ssh -o "StrictHostKeyChecking no" devopscourse@ssh.pythonanywhere.com << EOF
+    cd /home/devopscourse/OnlineDevops; /home/devopscourse/OnlineDevops/rebuild.sh
 EOF
