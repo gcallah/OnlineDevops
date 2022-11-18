288a4cf5db73ab7a4ac57a4559b37eef31ba248a
gcallah <gcallah@mac.com>
2022-11-17 23:38:52 -0500

    Took out hard-coded password.

--- qexport.sh
+++ qexport.sh
@@ -4,7 +4,7 @@ export SECRET_KEY='@+%x+r-cfv@*2+qi)&9w^6jvp-h=bypm+7$uee8jlm1%=qvcjw'
 export ENGINE='django.db.backends.mysql'
 export NAME='devopscourse$devopscourse'
 export USER='devopscourse'
-export PASSWORD='releaseit!'
+export PASSWORD=$DEVOPS_PASSWD
 export HOST='devopscourse.mysql.pythonanywhere-services.com'
 
 
