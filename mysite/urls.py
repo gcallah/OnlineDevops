723356f0403b51cfbc596389861b6891d73eef7e
gcallah <gcallah@mac.com>
2025-10-15 23:47:22 -0400

    Changing urls to match new Django setup.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,21 +5,20 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add an URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add an URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add an URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('devops.urls')),
-    url('participants/', include('participants.urls')),
-    url(r'^tinymce/', include('tinymce.urls'))
+    re_path(r'', include('devops.re_paths')),
+    re_path('participants/', include('participants.re_paths')),
+    re_path(r'^tinymce/', include('tinymce.re_paths'))
 ]
--- participants/urls.py
+++ participants/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import home
 
 urlpatterns = [
-    url('home', home, name='participant_home'),
-    url('login',
-        LoginView.as_view(template_name='participants_login_form.html'),
-        name='participant_login'),
-    url('logout',
-        LogoutView.as_view(
-            template_name='participant_logout_confirmation.html'),
-        name='participant_logout')
+    path('home', home, name='participant_home'),
+    path('login',
+         LoginView.as_view(template_name='participants_login_form.html'),
+         name='participant_login'),
+    path('logout',
+         LogoutView.as_view(
+             template_name='participant_logout_confirmation.html'),
+         name='participant_logout')
 ]
